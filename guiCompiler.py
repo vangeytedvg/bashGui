91c6e78a5dd6908ea90179b54b4b9b90ec59fba7
vangeytedvg <vangeytedvg@gmail.com>
2021-06-13 13:17:12 +0200

    Experimenting with gui compiler in python itself.

--- guiCompiler.py
+++ guiCompiler.py
@@ -7,12 +7,16 @@ for ui_file in os.listdir(os.path.curdir):
     # Loop through current directory
     if ui_file.endswith(".ui"):
         # We have a candidate file
-        print(f"Found {ui_file}")
-        base_name, _ = os.path.splitext(os.path.basename(ui_file))
-        target_name = f"frm_{str.capitalize(base_name)}.py"
-        print(target_name)
-        result = subprocess.run(["pyuic5", "-o", target_name, f"{ui_file}"], capture_output=True)
+        print(f"---> Found {ui_file}")
+        # get the filename without extension
+        base_filename, _ = os.path.splitext(os.path.basename(ui_file))
+        # Make the target name
+        target_filename = f"frm_{str.capitalize(base_filename)}.py"
+        # Run!
+        result = subprocess.run(["pyuic5", "-o", target_filename, f"{ui_file}"], capture_output=True)
         if result.returncode == 0:
-            print(f"Form compiled to {target_name}")
+            print(f"[v] Form compiled to {target_filename}")
         else:
-            print(f"An error occured {result}")
+            print(f"[e] An error occured {result}")
+
+print("Compilation terminated!")
